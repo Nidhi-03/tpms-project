d0134bc86b023fc3aa0abbde417b21e2b7ae4205
Nidhi-03 <git@stackblitz.com>
2024-11-11 22:39:35 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tpms-project
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Nidhi-03/tpms-project)
---
+++ script.js
@@ -0,0 +1,19 @@
+function addStudent() {
+  alert("Add Student button clicked");
+}
+
+function updateStudent() {
+  alert("Update Student button clicked");
+}
+
+function view() {
+  alert("View button clicked");
+}
+
+function changePassword() {
+  alert("Change Password button clicked");
+}
+
+function exit() {
+  alert("Exit button clicked");
+}
